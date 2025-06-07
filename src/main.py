765492f289f87968f7fa6cf4e91fad7155c8d122
Azarias Galama <azariasabera@gmail.com>
2025-06-07 13:15:37 +0300

    minor modification to main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from hydra.utils import instantiate
 @hydra.main(config_path="../conf", config_name="config", version_base=None)
 def main(cfg: DictConfig):
     builder = Builder(cfg)
-    builder.train_and_compare()
-
+    builder.build_and_compare()
+    
 if __name__ == "__main__":
     main()
